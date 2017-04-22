1b942dec4d5ad181e14b849730dfc4624abf3149
Gordo, Michael <Michael.Gordo@viacomcontractor.com>
2017-04-22 10:58:59 -0400

    updated test

--- test_app.py
+++ test_app.py
@@ -6,7 +6,15 @@ class TestApp(unittest.TestCase):
         ra = rangea.RangeAnalyser()
         ra.analyse([
             {'numbers':[1, 2, 3, 4, 5]},
+            {'numbers':[2, 2, 1, 4, 6]},
             {'numbers':[3, 2, 1, 4, 6]}
         ])
 
-        self.assertEqual([0, 3], ra.check([4, 2, 3, 5, 5]))
+        self.assertEqual({"min": 1, "max": 3}, ra.cache[1], "Range Analyse failed")
+        self.assertEqual({"min": 2, "max": 2}, ra.cache[2], "Range Analyse failed")
+        self.assertEqual({"min": 1, "max": 3}, ra.cache[3], "Range Analyse failed")
+        self.assertEqual({"min": 4, "max": 4}, ra.cache[4], "Range Analyse failed")
+        self.assertEqual({"min": 5, "max": 6}, ra.cache[5], "Range Analyse failed")
+
+        self.assertEqual([0, 3], ra.check([4, 2, 3, 5, 5]), "Range Check failed")
+        self.assertEqual([1, 2, 4], ra.check([3, 4, 7, 4, 0]), "Range Check failed")
